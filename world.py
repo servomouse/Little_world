348565f0e26f37e3ac66d7f74495a36462b9bd66
Servomouse <servomouse@protonmail.com>
2024-06-30 19:39:25 -0600

    Fix discriminant

--- world.py
+++ world.py
@@ -26,6 +26,46 @@ async def send_data(websocket, data):
     # print(f"Data sent {data}")
 
 
+def get_D(pov, screen, center, r):
+    """ Discriminant
+    pov, screen, center - coordinates like [x, y, z] """
+    x0 = pov[0]
+    x1 = screen[0]
+    y0 = pov[1]
+    y1 = screen[1]
+    z0 = pov[2]
+    z1 = screen[2]
+    d = (y1 - y0) / (x1 - x0)
+    e = (z1 - z0) / (x1 - x0)
+    f = d * x0 + y0
+    g = e * x0 + y0
+    a = 1 + d**2 + e**2
+    b = (-2 * d * f) - (2 * e * g)
+    c = f**2 + g**2 - r**2
+    D = b**2 - 4 * a * c
+    return D
+
+
+def get_Distance(pov, screen, center, r):
+    """ Minimal distance
+    pov, screen, center - coordinates like [x, y, z] """
+    x0 = pov[0]
+    x1 = screen[0]
+    y0 = pov[1]
+    y1 = screen[1]
+    z0 = pov[2]
+    z1 = screen[2]
+    a = y1 - y0
+    b = x1 - x0
+    c = z1 - z0
+    s = [b, a, c]
+    M1 = [x0, y0, z0]   # Center of the sphere
+    M0M1 = [x0, y0, z0]
+    M0M1s = [M0M1[1] * s[2] - M0M1[2] * s[1], M0M1[0] * s[2] - M0M1[2] * s[0], M0M1[0] * s[1] - M0M1[1] * s[0]]
+    D = sqrt(M0M1s[0]**2 + M0M1s[1]**2 + M0M1s[2]**2) / sqrt(s[0]**2 + s[1]**2 + s[2]**2)
+    return D
+
+
 async def hello():
     data = import_data('variables.js')
 
@@ -52,25 +92,8 @@ async def hello():
             z1 = z * cell_size
             for y in range(-h_width, h_width, 1):
                 y1 = y * cell_size
-                x0 = pov[0]
-                x1 = screen[0]
-                y0 = pov[1]
-                z0 = pov[2]
-                a = y1 - y0
-                b = x1 - x0
-                c = z1 - z0
-                # f = a/b
-                # g = c/b
-                # d = (f*x0) + y0
-                # e = (g*x0) + z0
-                # D = (2*d*f - 2*e*g)**2 - 4 * (1 + f**2 + g**2) * (d**2 + e**2 - r**2)
-                # if D >= 0:
-                s = [b, a, c]
-                M1 = [x0, y0, z0]
-                M0M1 = [x0, y0, z0]
-                M0M1s = [M0M1[1] * s[2] - M0M1[2] * s[1], M0M1[0] * s[2] - M0M1[2] * s[0], M0M1[0] * s[1] - M0M1[1] * s[0]]
-                D = sqrt(M0M1s[0]**2 + M0M1s[1]**2 + M0M1s[2]**2) / sqrt(s[0]**2 + s[1]**2 + s[2]**2)
-                if D > r:
+                D = get_D(pov, [screen[0], y1, z1], [], r)
+                if D < 0:
                     cells_data.append([y + h_width, z + h_height, dark_color])
                 else:
                     cells_data.append([y + h_width, z + h_height, bright_color])
